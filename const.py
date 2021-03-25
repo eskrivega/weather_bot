36c2ffe5888cd40d0a54bad136b5c6b630363d48
eskrivega <71096102+eskrivega@users.noreply.github.com>
2021-03-25 22:15:09 +0300

    Update const.py

--- const.py
+++ const.py
@@ -1,10 +1,10 @@
-TOKEN = '1726245951:AAHajR0q3ZEXPpEcl9IOUpYmZYbDWwlnils'
+TOKEN = 'token'
 URL = 'https://api.telegram.org/bot{token}/{method}'
 
 UPDATE_METH = 'getUpdates'
 SEND_METH = 'sendMessage'
 
-MY_ID = 336590651
+MY_ID = id
 
 UPDATE_ID_FILE_PATH = 'update_id'
 
@@ -16,6 +16,6 @@ with open(UPDATE_ID_FILE_PATH) as file:
     UPDATE_ID = data
 
 
-WEATHER_TOKEN = '6c4e05f8106b0e9b8c6b30528efdff19'
+WEATHER_TOKEN = 'token'
 
 WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}'
